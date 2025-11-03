ee47053f1296b2691d4170b65e1b8223f884af93
minnielach <165672835+minnielach@users.noreply.github.com>
2025-11-03 13:35:18 +1030

    I have seperated the code into React files/containers. I have added to the DJControls.jsx file for some features/components such as a volume slider and setting the CPM (functuality does not work still as still just basic and just the physical component for now).

---
+++ src/components/DJControls.jsx
@@ -0,0 +1,35 @@
+function DJControls() {
+    return (
+        <>
+            <div className="input-group mb-3">
+            <span className="input-group-text" id="cpm_label">setCPM</span>
+            <input type="text" className="form-control" id="cpm_text_input"placeholder="120" aria-label="CPM" aria-describedby="cpm_label"/>
+            </div>
+
+            <label for="volume_range" className="form-label">Volume</label>
+            <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
+
+            <div className="form-check"> 
+                <input className="form-check-input" type="checkbox" value="" id="s1"/> 
+                <label className="form-check-label" for="s1">
+                     s1
+                </label> 
+            </div> 
+            <div className="form-check"> 
+                <input className="form-check-input" type="checkbox" value="" id="d1"/> 
+                <label className="form-check-label" for="d1">
+                     d1
+                </label> 
+            </div> 
+            <div className="form-check"> 
+                <input className="form-check-input" type="checkbox" value="" id="d2"/> 
+                <label className="form-check-label" for="d2">
+                     d2
+                </label> 
+            </div> 
+            
+    </>
+    );
+}
+
+export default DJControls;
