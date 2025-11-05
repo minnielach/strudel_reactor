76013a4cf1acc068650a151bc6f2f92d27f4fb95
minnielach <165672835+minnielach@users.noreply.github.com>
2025-11-06 00:09:44 +1030

    Changed the javascript code to a react based code to align with the assignment requirements for play, stop and preprocess buttons.

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -6,24 +6,24 @@ function DJControls() {
             <input type="text" className="form-control" id="cpm_text_input"placeholder="120" aria-label="CPM" aria-describedby="cpm_label"/>
             </div>
 
-            <label for="volume_range" className="form-label">Volume</label>
+            <label htmlFor="volume_range" className="form-label">Volume</label>
             <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
 
             <div className="form-check"> 
                 <input className="form-check-input" type="checkbox" value="" id="s1"/> 
-                <label className="form-check-label" for="s1">
+                <label className="form-check-label" htmlFor="s1">
                      s1
                 </label> 
             </div> 
             <div className="form-check"> 
                 <input className="form-check-input" type="checkbox" value="" id="d1"/> 
-                <label className="form-check-label" for="d1">
+                <label className="form-check-label" htmlFor="d1">
                      d1
                 </label> 
             </div> 
             <div className="form-check"> 
                 <input className="form-check-input" type="checkbox" value="" id="d2"/> 
-                <label className="form-check-label" for="d2">
+                <label className="form-check-label" htmlFor="d2">
                      d2
                 </label> 
             </div> 
